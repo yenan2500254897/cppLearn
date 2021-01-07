af7a97973ed85059d28bfd6bebadab1e624e63b6
yenan2500254897 <2500254897@qq.com>
2021-01-07 12:41:11 +0800

    878. 第 N 个神奇数字----二分查找（用例全过，但是超时）

--- cppPrimerLearn/leetcode/2021.01.07/nthMagicalNumber.cpp
+++ cppPrimerLearn/leetcode/2021.01.07/nthMagicalNumber.cpp
@@ -7,6 +7,8 @@
 如果正整数可以被 A 或 B 整除，那么它是神奇的。
 
 返回第 N 个神奇数字。由于答案可能非常大，返回它模 10^9 + 7 的结果。
+思路：每a*b个数为一个循环，只需要算出[1...(a*b)]中有多少个数符合要求，且排好顺序即可，假设区间[1...(a*b)]满足的数的个数为len，
+只需用n对len做除法和区域，算出第n个数在哪一个区间的第几个即可。
 */
 using namespace std;
 
@@ -18,17 +20,22 @@ int nthMagicalNumber(int n, int a, int b)
         a = b;
         b = value;
     }
+    if(n==1)
+    {
+        return a;
+    }
 
     long limit = (long)a*(long)b;
     set<long> record;
-    set<long> temp;
-    int len = record.size();
+    set<long> assistant;
     record.insert(a);
     record.insert(b);
-    while(len!=record.size())
+    assistant.insert(a);
+    assistant.insert(b);
+    set<long> temp;
+    while(!assistant.empty())
     {
-        len = record.size();
-        for(auto item:record)
+        for(auto item:assistant)
         {
             int nextValue1 = item+a;
             int nextValue2 = item+b;
@@ -41,7 +48,9 @@ int nthMagicalNumber(int n, int a, int b)
                 temp.insert(nextValue2);
             }
         }
-        record.insert(temp.begin(), temp.end());
+        record.insert(assistant.begin(), assistant.end());
+        assistant.swap(temp);
+        temp.clear();
     }
 
     long divisor = record.size();
@@ -50,9 +59,17 @@ int nthMagicalNumber(int n, int a, int b)
 
     long result = 0;
     result += circleNumber*limit;
-    vector<long> assistant;
-    assistant.insert(assistant.end(), record.begin(), record.end());
-    result += assistant[reserveNumber];
+    // set<long>::iterator iter = record.begin();
+    // int k = 0;
+    // while(k<reserveNumber)
+    // {
+    //     k++;
+    //     iter++;
+    // }
+    //result += (*iter);
+    vector<long> tempVec;
+    tempVec.insert(tempVec.end(), record.begin(), record.end());
+    result += tempVec[reserveNumber];
     return result%1000000007;
 }
 
@@ -64,12 +81,12 @@ int main()
     // int n=4;
     // int a=2;
     // int b=3;
-    // int n=5;
-    // int a=2;
-    // int b=4;
-    int n=3;
-    int a=6;
+    int n=5;
+    int a=2;
     int b=4;
+    // int n=3;
+    // int a=6;
+    // int b=4;
     cout<<nthMagicalNumber(n, a, b)<<endl;
     return 0;
 
