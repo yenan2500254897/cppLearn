9e787c08db0de2e8132d43c12362d2e79408131d
yenan2500254897 <2500254897@qq.com>
2021-01-09 21:17:05 +0800

    1531. 压缩字符串 II----字符串（AC,参考的题解）

--- cppPrimerLearn/leetcode/2021.01.09/getLengthOfOptimalCompression.cpp
+++ cppPrimerLearn/leetcode/2021.01.09/getLengthOfOptimalCompression.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 /*
 1531. 压缩字符串 II
@@ -11,146 +12,69 @@
 
 请你返回删除最多 k 个字符后，s 行程长度编码的最小长度 。
 
+思路：dp[i][j]表示s[1..i](s的下标从1开始)，删除j个字符后，s 行程长度编码的最小长度 。
+当考虑第i+1个字符时，如果不保留，则dp[i][j] = dp[i-1][j-1];
+如果考虑保留，则考虑与索引i之前且等于s[i]的字符pre合并，pre的特征是和s[i]之间最多隔j个不同的字母，转移方程见67行
+
 来源：力扣（LeetCode）
 */
 using namespace std;
 
-int result = INT_MAX;
-
-char findChar(string& s)
+int cost(int value)
 {
-    if(s.empty())
-    {
-        return ' ';
-    }
-    for(int i=s.size()-1;i>=0;i--)
-    {
-        if(s[i]>='a' && s[i]<='z')
-        {
-            return s[i];
-        }
-    }
-    return ' ';
-}
-
-int findNumber(string& s)
-{
-    if(s.empty())
-    {
-        return 0;
-    }
-
-    if(s.back()>='a' && s.back()<='z')
+    if(value == 1)
     {
         return 1;
-    }
-
-    string str = "";
-    for(int i=s.size()-1;i>=0;i--)
+    }else if(value<10)
     {
-        if(s[i]>='a' && s[i]<='z')
-        {
-            break;
-        }
-        str.insert(str.begin(), s[i]);
-    }
-    return atoi(str.c_str());
-}
-
-void findSolution(string& s, int index, int remainK, string compressStr)
-{
-    //cout<<compressStr<<endl;
-    if(index == s.size())
+        return 2;
+    }else if(value<100)
     {
-        //cout<<compressStr<<endl;
-        int len = compressStr.size();
-        result = min(result, len);
-        return;
-    }
-    if(compressStr.size()>result)
+        return 3;
+    }else
     {
-        return;
+        return 4;
     }
-
-    int nextCharIndex = index+1;
-    while(nextCharIndex<s.size() && s[index] == s[nextCharIndex])
-    {
-        nextCharIndex++;
-    }
-
-    char cur = s[index];
-    int curNumber = nextCharIndex-index;
-    string temp = "";
-    for(int i=0;i<=min(remainK, curNumber);i++)
-    {
-        temp = compressStr;
-        int diff = curNumber - i;
-        if(diff>0)
-        {
-            temp.insert(temp.end(), cur);
-            if(diff>1)
-            {
-                temp += to_string(diff);
-            }
-        }
-        findSolution(s, nextCharIndex, remainK-i, temp);
-
-    }
+    return 0;
     
-
 }
-
-//index每次只往前走一部，会导致大量重复计算
-// void findSolution(string& s, int index, int remainK, string compressStr)
-// {
-//     //cout<<compressStr<<endl;
-//     if(index == s.size())
-//     {
-//         //cout<<compressStr<<endl;
-//         int len = compressStr.size();
-//         result = min(result, len);
-//         return;
-//     }
-//     if(compressStr.size()>result)
-//     {
-//         return;
-//     }
-
-//     char cur = s[index];
-//     char pre = findChar(compressStr);
-//     int number = findNumber(compressStr);
-
-//     //删除cur
-//     if(remainK>0)
-//     {
-//         findSolution(s, index+1, remainK-1, compressStr);
-//     }
-
-//     //保留cur
-//     if(pre == ' ' || pre != cur)
-//     {
-//         compressStr.insert(compressStr.end(), cur);
-//     }
-//     else
-//     {
-//         if(number>1)
-//         {
-//             string numberStr = to_string(number);
-//             compressStr.erase(compressStr.size()-numberStr.size(), numberStr.size());
-//             //cout<<"1: "<<compressStr<<endl;
-//         } 
-//         string newNumberStr = to_string(number+1);
-//         compressStr += newNumberStr; 
-//         //cout<<"2: "<<compressStr<<endl;
-//     }
-//     findSolution(s, index+1, remainK, compressStr);
-
-// }
 
 int getLengthOfOptimalCompression(string s, int k) 
 {
-    findSolution(s, 0, k, "");
-    return result;
+    int len = s.size();
+    if(k==len)
+    {
+        return 0;
+    }
+    vector<vector<int>> dp(len+1, vector<int>(k+1, INT_MAX>>1));
+    dp[0][0] = 0;
+    
+    for(int i=1;i<=len;i++)
+    {
+        for(int j=0;j<=k&&j<=i;j++)
+        {
+            if(j-1>=0)
+            {
+                dp[i][j] = dp[i-1][j-1];
+            }
+            int diff = 0;
+            int common = 0;
+            for(int l=i;l>=1 && diff<=j;l--)
+            {
+                if(s[i-1] == s[l-1])
+                {
+                    common++;
+                    dp[i][j] = min(dp[i][j], dp[l-1][j-diff]+cost(common));
+                }
+                else
+                {
+                    diff++;
+                }
+                
+            }
+        }
+    }
+    return dp[len][k];
 }
 
 int main()
