6651168eb702b53936b24a683007f19c6bd8c41c
yenan2500254897 <2500254897@qq.com>
2021-01-09 18:31:03 +0800

    1542. 找出最长的超赞子字符串----字符串（超时）

--- cppPrimerLearn/leetcode/2021.01.09/longestAwesome.cpp
+++ cppPrimerLearn/leetcode/2021.01.09/longestAwesome.cpp
@@ -11,48 +11,56 @@
 该字符串是 s 的一个非空子字符串
 进行任意次数的字符交换后，该字符串可以变成一个回文字符串
 
+//用树状数组来判断一个区间是否可以变为回文字符串 ？未成功
+思路：一次判断s中是否存在长度为len的回文字符串
+用二进制数字number表示0-9中各个数字的奇偶性，如果number中存在2个以上的1，说明存在两个不同的数有奇数个，显然不可能是回文字符串。
 来源：力扣（LeetCode）
 */
 using namespace std;
 
 int result = 0;
 
-bool isValid(vector<int>& input)
-{
-    int len = input.size();
-    int number = 0;
-    for(int i=0;i<len;i++)
-    {
-        if(input[i]%2 == 1)
-        {
-            if(number>0)
-            {
-                return false;
-            }
-            number++;
-        }
-    }
-    return true;
-}
+// bool isValid(vector<int>& assistant)
+// {
+//     int total = 0;
+//     for(int i=0;i<assistant.size();i++)
+//     {
+//         total += assistant[i];
+//         if(total>=2)
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
+bool isValid(int value)
+{
+    return __builtin_popcount(value)<2;
+}
 bool isAwesome(string& s, int len)
 {
-    vector<int> assistant(10, 0);
+    int value = 0;
     for(int i=0;i<len;i++)
     {
-        assistant[s[i]-'0']++;
+        char cur = s[i];
+        int index = cur-'0';
+        value ^= (1<<index);
     }
-    if(isValid(assistant))
+    if(isValid(value))
     {
         result = len;
         return true;
     }
+    //性能损耗在这里
     for(int j=1;j+len-1<s.size();j++)
     {
-        assistant[s[j-1]-'0']--;
-        assistant[s[j+len-1]-'0']++;
+        int pre = s[j-1]-'0';
+        int next = s[j+len-1]-'0';
+        value ^= (1<<pre);
+        value ^= (1<<next);
         //这里是改进点，不一定要isValid
-        if(isValid(assistant))
+        if(isValid(value))
         {
             result = len;
             return true;
