53d24ee7956b3ad3c5b7f1ae2375acc8ecd03f9c
yenan2500254897 <2500254897@qq.com>
2021-01-12 22:17:23 +0800

    1680. 连接连续二进制数字----数学（Medium，AC）

--- cppPrimerLearn/leetcode/2021.01.12/concatenatedBinary.cpp
+++ cppPrimerLearn/leetcode/2021.01.12/concatenatedBinary.cpp
@@ -9,6 +9,8 @@
 思路：难点在于怎么通过之前的数字对应的字符串快速推导出接下来的数字对应的字符串
 规律：可以发现[pow(2,i)+1...pow(2,i+1))的整数对应的字符串可以由pow(2,i)对应的字符串和[pow(2, i-1), pow(2, i))拼接而来
 特殊的是当为2的整数次方时，由pow(2, i)对应的字符和"0"拼接而来。
+
+改进：不用字符串，直接可以进行数值的相加。
 */
 using namespace std;
 int concatenatedBinary(int n) 
@@ -19,41 +21,18 @@ int concatenatedBinary(int n)
         record.push_back(record.back()*2);
     }
 
-    vector<string> dp;
-    dp.push_back("0");
-    dp.push_back("1");
-
-    string preStr = "1";
-    string curStr = " ";
     int len = 1;
-    int leftIndex = 0;
+    int result = 1;
+    int mod=1e9+7;
     for(int i=2;i<=n;i++)
     {
         if(i == record[len])
         {
-            curStr = preStr + "0";
-            preStr = curStr;
             len++;
-            leftIndex = 1;
         }
-        else
-        {
-            curStr = preStr.substr(0, preStr.size()-dp[leftIndex].size()) + dp[leftIndex];
-            leftIndex++;
-        }
-        dp.push_back(curStr);
-        // cout<<"i:="<<i<<"   curStr:="<<curStr<<endl;
+        result = (((long long)result<<len) + i)%mod;
     }
 
-    int result = 0;
-    int mod=1e9+7;
-    for(string s:dp)
-    {
-        for(char ch:s)
-        {
-            result = ((long long)result*2 + (ch - '0'))%mod;
-        }
-    }
     return result;
 }
 
